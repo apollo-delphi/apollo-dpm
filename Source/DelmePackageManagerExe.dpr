96d0e40fb29c5ae256f8afb99dabd6d2df69e535
alezzle <niekras@tut.by>
2019-09-14 18:01:41 +0300

    The first commit

---
+++ Source/DelmePackageManagerExe.dpr
@@ -0,0 +1,14 @@
+program DelmePackageManagerExe;
+
+uses
+  Vcl.Forms,
+  ConsoleForm in 'ConsoleForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
